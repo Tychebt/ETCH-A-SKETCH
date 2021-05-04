a0b80e048b60b9b4b5142e97225514a91479a4d1
Tychebt <masse.ju35@gmail.com>
2021-05-04 15:42:20 +0200

    grid fait

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-alert('hello')
+function genDivs(v){ 
+    var e = document.querySelector('.container'); // whatever you want to append the rows to: 
+    for(var i = 0; i < v; i++){ 
+      var row = document.createElement("div"); 
+      row.className = "row"; 
+      for(var x = 1; x <= v; x++){ 
+          var cell = document.createElement("div"); 
+          cell.className = "gridsquare"; 
+          cell.innerText = (i * v) + x;
+          row.appendChild(cell); 
+      } 
+      e.appendChild(row); 
+    } 
+    //document.getElementById("code").innerText = e.innerHTML;
+
+  }
