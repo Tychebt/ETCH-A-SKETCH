158e4065b3681d96e2daef39f6fdc876f2192c2a
Tychebt <masse.ju35@gmail.com>
2021-05-05 14:01:24 +0200

    random color

--- script.js
+++ script.js
@@ -13,7 +13,8 @@
 
           row.appendChild(cell); 
           cell.addEventListener("mouseover",function(event){
-            event.target.style.background = "orange";
+            var randomColor = "#"+Math.floor(Math.random()*16777215).toString(16);
+            event.target.style.background = `${randomColor}`
           }, false);
       } 
       e.appendChild(row); 
