f1b79c9a239b007e62a38527f7fc9a363a7dea3b
Tychebt <masse.ju35@gmail.com>
2021-05-05 12:31:50 +0200

    ne reste plus que le reset

--- script.js
+++ script.js
@@ -1,13 +1,16 @@
 
+    let value = prompt("How many cells in you table ?", 16)
+    
     var e = document.querySelector('.container'); // whatever you want to append the rows to: 
     e.style.display = 'table-cell';
-    for(var i = 0; i < 16; i++){ 
+    for(var i = 0; i < value; i++){ 
       var row = document.createElement("div"); 
       row.className = "row"; 
-      for(var x = 1; x <= 16; x++){ 
+      for(var x = 1; x <= value; x++){ 
           var cell = document.createElement("div"); 
           cell.className = "gridsquare"; 
-         
+          cell.style.height = `calc(960px/${value})`
+
           row.appendChild(cell); 
           cell.addEventListener("mouseover",function(event){
             event.target.style.background = "orange";
@@ -15,5 +18,15 @@
       } 
       e.appendChild(row); 
     } 
+           
 
- 
+    let reset = document.querySelector('.reset')
+    reset.addEventListener("click",function(event){
+  
+
+      for(let k = 0; k<value; k++){
+        e.removeChild(row); 
+      }
+      
+     
+          }, false);
